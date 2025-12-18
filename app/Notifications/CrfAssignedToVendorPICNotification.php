61fb598517a1508e2752401f38ee084d2b4ffa26
Haqimi Imran <haqimiah05@gmail.com>
2025-12-18 11:37:29 +0800

    updated notification

--- app/Notifications/CrfAssignedToVendorPICNotification.php
+++ app/Notifications/CrfAssignedToVendorPICNotification.php
@@ -28,7 +28,7 @@ class CrfAssignedToVendorPICNotification extends Notification
             'title' => 'New CRF Assigned to You',
             'message' => "CRF #{$this->crf->id} for {$this->crf->fname} has been assigned to you by Vendor Admin.",
             'crf_id' => $this->crf->id,
-            'url' => url('/crfs/' . $this->crf->id),
+            'action_url' => url('/crfs/' . $this->crf->id),
             'type' => 'crf_assigned_vendor_pic',
             'created_at' => now()->toDateTimeString(),
         ];
