1635d9e48440eb330525a2da1dbcc6c3309df7c3
Haqimi Imran <haqimiah05@gmail.com>
2026-02-10 16:20:09 +0800

    changed logo color

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -3,7 +3,7 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <div className="bg-blue-600 p-2 rounded-lg">
+        <div className="bg-gradient-to-br from-blue-600 to-blue-400 p-2 rounded-lg">
             <FileText className="h-6 w-6 text-white" />
         </div>
     );
